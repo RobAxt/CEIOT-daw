252102ec549bc56beefccc880d72f5b26b4ab0a2
RobAxt <roberto.axt@gmail.com>
2022-07-04 22:02:56 -0300

    Clase 2

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -2,5 +2,20 @@
 function SayHello(){
     let current_value = document.getElementById("textarea_1") as HTMLInputElement;
     let new_value = "Hello world!!!" + "\n" + current_value.value;
-    document.getElementById("textarea_1").innerHTML = new_value;
+    document.getElementById("textarea_1").innerHTML = new_value ;
 }
+
+class Main {
+    public usr: Usuario;
+    constructor() {
+        this.usr =  new Usuario("Roberto",42);
+        //console.log(this.usr.mostrar());
+    }
+}
+function inicio() {    
+    let main:Main = new Main();
+    let texto = document.getElementById("textarea_1");
+    texto.innerHTML = main.usr.mostrar();
+}
+
+window.onload = inicio;
