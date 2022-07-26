c4beb9d23dba00f4bd64a5f7ca85eee2adde595b
RobAxt <roberto.axt@gmail.com>
2022-07-25 22:07:20 -0300

    Clase 5

---
+++ src/backend/datos.js
@@ -0,0 +1,1 @@
+module.exports={nombre:'Brian',apellido:'Ducca'}
--- src/backend/index.js
+++ src/backend/index.js
@@ -5,6 +5,7 @@ var PORT    = 3000;
 var express = require('express');
 var app     = express();
 var utils   = require('./mysql-connector');
+var fs      = require('fs');
 
 // to parse application/json
 app.use(express.json()); 
@@ -30,11 +31,55 @@ app.get('/devices/', function(req, res, next) {
             'type': 2, 
         },
     ]
+   
     res.send(JSON.stringify(devices)).status(200);
 });
+
+app.get('/dato/', function(req, res, next) {
+
+    fs.readFile('/home/node/app/src/datos.json',(err, data)=> {
+        if (err) {
+            console.error(err);
+            return;
+          }
+        data = JSON.parse(data);
+        console.log(JSON.stringify(data));
+        res.json(data).status(200);
+    });   
+    
+});
+
+app.get('/devices/:id/:state', function(req, res, next) {
+    res.send("Parametros: " + req.params.id + " " + req.params.state); 
+}); 
+
 
 app.listen(PORT, function(req, res) {
     console.log("NodeJS API running correctly");
 });
 
+// //============================================================================
+// //import de modulo eventos
+// const eventos= require('events');
+
+// //Creo objeto eventEmitter
+// const eventEmitter = new eventos.EventEmitter();
+
+// //Creo una cuncion handlre que se encarga de manejar un evento
+// const connectHandler = function connected(){
+//     console.log("Conexion exitosa");
+//     eventEmitter.emit("data_recibida");
+// }
+
+// //Bindeo el evento conexion al handlee creado anteiormente
+// eventEmitter.on('conexion',connectHandler);
+// //Bindeo el evento con funcion anonima
+// eventEmitter.on('data_recibida', function(){
+//     console.log("Llego data");
+// });
+// //emito evento
+// eventEmitter.emit('conexion');
+//=============================================================================
+// let persona = require('./datos.js');
+// console.log(persona.nombre + ' ' + persona.apellido);
 //=======[ End of file ]=======================================================
