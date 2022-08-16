4d4a5e65f820d85ec1a8ad2b3c018986e968eed6
RobAxt <roberto.axt@gmail.com>
2022-08-15 23:32:02 -0300

    database dump

--- db/dumps/smart_home.sql
+++ db/dumps/smart_home.sql
@@ -20,7 +20,9 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `smart_home`
 --
+CREATE DATABASE IF NOT EXISTS smart_home;
 
+USE smart_home;
 -- --------------------------------------------------------
 
 --
